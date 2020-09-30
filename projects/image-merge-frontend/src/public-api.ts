a4a2210980fdaac2bd6e67881b09662491582f66
Martin Ruf <martin.f.ruf@gmail.com>
2020-09-30 21:49:38 +0200

    [master] fixed import

--- projects/image-merge-frontend/src/public-api.ts
+++ projects/image-merge-frontend/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of image-merge-frontend
  */
 
-export * from 'projects/image-merge-frontend/src/lib/models/layer-object.interface';
+export * from './lib/models/layer-object.interface';
 export * from './lib/image-merge-frontend.component';
 export * from './lib/image-merge-frontend.module';
