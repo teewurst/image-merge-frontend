c9fbd7f17735f6129be38915dfa1cefa65a543d8
Martin Ruf <martin.ruf@check24.de>
2020-12-03 22:28:09 +0100

    [master] Added binding to event listener

--- projects/image-merge-frontend/src/lib/directive/hide-image-on-lode.directive.ts
+++ projects/image-merge-frontend/src/lib/directive/hide-image-on-lode.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Directive, ElementRef, Input, OnChanges, OnDestroy, OnInit, Renderer2, SimpleChanges} from '@angular/core';
 
 @Directive({
   selector: '[libHideImageOnLode]'
@@ -8,10 +8,12 @@ export class HideImageOnLodeDirective implements OnInit, OnDestroy, OnChanges{
   @Input()
   public src: string;
 
-  constructor(private elRef: ElementRef) {}
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+    this.renderer.setAttribute(this.elRef.nativeElement, 'src', this.src);
+  }
 
   public ngOnInit(): void {
-    this.elRef.nativeElement.addEventListener('load', this.showElement);
+    this.elRef.nativeElement.addEventListener('load', this.showElement.bind(this));
   }
 
   public ngOnDestroy(): void {
@@ -19,7 +21,8 @@ export class HideImageOnLodeDirective implements OnInit, OnDestroy, OnChanges{
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
-    if (changes.imageSource && changes.imageSource.currentValue !== changes.imageSource.previousValue) {
+    if (changes.src && changes.src.currentValue !== changes.src.previousValue) {
+      this.renderer.setAttribute(this.elRef.nativeElement, 'src', this.src);
       this.elRef.nativeElement.style.display = 'none';
     }
   }
